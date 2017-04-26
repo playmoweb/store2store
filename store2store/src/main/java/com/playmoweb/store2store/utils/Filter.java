310d60645ae7642f06ff17a9738e334149e67708
Thibaud Giovannetti <tspoke@hotmail.fr>
2017-04-26 21:04:57 +0200

    Improve filter with generics

--- store2store/src/main/java/com/playmoweb/store2store/utils/Filter.java
+++ store2store/src/main/java/com/playmoweb/store2store/utils/Filter.java
@@ -8,34 +8,29 @@ import java.util.WeakHashMap;
 public class Filter extends WeakHashMap<String, Filter.KeyValuePair> {
     // TODO optimize access with a copy in an weak list
 
-    public Filter(String key, String value) {
+    public <T> Filter(String key, T value) {
         super();
         add(key, FilterType.EQUAL, value);
     }
 
-    public Filter(String key, FilterType type, String value) {
+    public <T> Filter(String key, FilterType type, T value) {
         super();
         add(key, type, value);
     }
 
-    public Filter(String key, int value) {
-        super();
-        add(key, FilterType.EQUAL, String.valueOf(value));
-    }
-
-    public Filter add(String key, FilterType type, String value) {
-        this.put(key, new KeyValuePair(type, value));
+    public <T> Filter add(String key, FilterType type, T value) {
+        this.put(key, new KeyValuePair<>(type, value));
         return this;
     }
 
     /**
-     * Optimized immutable KeyValuePair class
+     * Immutable KeyValuePair class
      */
-    public static class KeyValuePair {
+    public static class KeyValuePair<T> {
         public final FilterType filterType;
-        public final String value;
+        public final T value;
 
-        public KeyValuePair(FilterType ft, String v){
+        public KeyValuePair(FilterType ft, T v){
             value = v;
             filterType = ft;
         }
